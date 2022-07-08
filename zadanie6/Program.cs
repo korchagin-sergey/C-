85acaa933fbd3985794978194f3e69d7e406f44f
Sergey Korchagin <brodaga_777@mail.ru>
2022-07-08 09:48:36 +0400

    zadanie6 ветвления if и else

---
+++ zadanie6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("В Ведите имя пользоватеья: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура это же МаШа!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
